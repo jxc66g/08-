a81321c8e3fa2dc179fc76ab28d24c1cb3550f15
xc J <jxc66g@gmail.com>
2020-08-23 15:50:00 +0800

    添加项目文件。

---
+++ 07 指针和函数.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+//实现两个数字进行交换
+void swap01(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void swap02(int* p1, int* p2)
+{
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+
+int main7() {
+
+	//指针和函数
+
+	//1.值传递
+	int a = 10;
+	int b = 20;
+	  //swap01(a, b);
+
+	//2.地址传递
+	//如果是地址传递，可以修饰实参
+	swap02(&a, &b);
+
+
+	system("pause");
+	return 0;
+}
